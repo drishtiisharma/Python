d3b5819cd9697bb77e92d21d03b764af0159be83
Drishti Sharma <118450578+drishtiisharma@users.noreply.github.com>
2026-08-26 14:57:25 +0530

    Add files via upload

---
+++ Learning/sixth.py
@@ -0,0 +1,7 @@
+import sys
+# the output will be a list of directory paths where python looks for whenever we use the import statement
+# print(sys.path) # shows the module search paths
+# print(sys.version) # shows python version available
+print(sys.argv) # 
+# print(sys.modules) # contains modules alr loaded
+# print(sys.exit())
