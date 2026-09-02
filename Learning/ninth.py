9fef83357b99a61e407fc6d8dbaf02e1390bf136
drishtiisharma <drishti.1106@gmail.com>
2026-09-02 17:40:32 +0530

    multiple deco

--- Learning/ninth.py
+++ Learning/ninth.py
@@ -11,15 +11,38 @@
 
 ## decorator with func args
 
-def logger(func):
+# def logger(func):
+
+#     def wrapper(name):
+#         print("wrapper func called...")
+#         return func(name)
+#     return wrapper
+
+# @logger
+# def greet(name):
+#     return "hello " + name
+
+# print(greet("drishti"))
+
+## multiple decorators
+
+def log1(func):
 
     def wrapper(name):
-        print("wrapper func called...")
+        print("log1 checking...")
         return func(name)
     return wrapper
 
-@logger
-def greet(name):
-    return "hello " + name
+def log2(func):
 
-print(greet("drishti"))
+    def wrapper2(name):
+        print("log2 checking...")
+        return func(name)
+    return wrapper2
+
+@log1
+@log2
+def greet(name):
+    return "hellooo " + name
+
+print(greet('drishti'))
