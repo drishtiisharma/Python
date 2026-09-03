40069fb1fe9c33542208521fcfbf9c6bf5908f82
drishtiisharma <drishti.1106@gmail.com>
2026-09-04 05:00:58 +0530

    recurision

---
+++ Practice/recursion.py
@@ -0,0 +1,70 @@
+# def count(n):
+#     if n == 0:
+#         return
+#     else:
+#         count(n-1)
+#         print(n)
+
+# n = int(input())
+# count(n)
+
+# def rev(n):
+#     if n == 0:
+#         return
+#     else:
+#         print(n)
+#         rev(n-1)
+# rev(10)
+
+# def fact(n):
+#     if n in [0,1]:
+#         return 1
+#     else:
+#         return n * fact(n-1)
+    
+# print(fact(5))
+
+# def sum(n):
+#     if n == 0:
+#         return 0
+#     else:
+#         return n + sum(n-1)
+ 
+# print(sum(10))
+
+# def sdigits(n):
+#     if n == 0:
+#         return 0
+#     else:
+#         return n % 10 + sdigits(n//10)
+    
+# print(sdigits(123))
+
+# def count(n):
+#     if n == 0:
+#         return 0
+#     else:
+#         return 1 + count(n//10)
+# print(count(456))
+
+# def p(a,b):
+#     if b == 0:
+#         return 1
+#     else:
+#         return a * p(a,b-1)
+# print(p(2,3))
+
+# def fib(n):
+#     if n <= 1:
+#         return n
+#     else:
+#         return fib(n-1) + fib(n-2)
+# for i in range(3):
+#     print(fib(i))
+
+
+    
+
+
+
+
