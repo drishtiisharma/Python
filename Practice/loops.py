d840ef93911cc0e5cdd151598a1d241fbfe280db
drishtiisharma <drishti.1106@gmail.com>
2026-09-03 17:23:59 +0530

    added cheatsheets

--- Practice/loops.py
+++ Practice/loops.py
@@ -25,3 +25,11 @@
 #     x //=10
 # print(sum)
 
+# n = int(input())
+# for i in range(1,n+1):
+#     sum = 0
+#     for j in range(1,i+1):
+#         sum+=j
+#         print(j,end='')
+#     print(':',sum)
+#     print()
